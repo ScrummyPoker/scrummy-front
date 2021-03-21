74af4cd3e9398b77481746a2c2b4e29de5e11f5d
vitor <santos_vitao@hotmail.com>
2021-03-20 21:21:28 -0300

    add prettier config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
-import io from "socket.io-client";
+import React, { useEffect } from 'react';
+import io from 'socket.io-client';
 
 function App() {
   useEffect(() => {
-    const socket = io("http://localhost:3000", { transports: ["websocket"] });
-    socket.on("connect", () => console.log("Front connected"));
+    const socket = io('http://localhost:3000', { transports: ['websocket'] });
+    socket.on('connect', () => console.log('Front connected'));
   }, []);
 
   return (
