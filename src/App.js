668f7a5ac165ca23bfa812538c6872ef2a6627ba
vitor <santos_vitao@hotmail.com>
2021-03-20 21:06:28 -0300

    add connect to socketio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import io from "socket.io-client";
 
 function App() {
+  useEffect(() => {
+    const socket = io("http://localhost:3000", { transports: ["websocket"] });
+    socket.on("connect", () => console.log("Front connected"));
+  }, []);
+
   return (
     <div>
       Hello World!
